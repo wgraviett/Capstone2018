4a9df36007387dd8b42f9d3796b0f9cba34391d3
wgraviett <wagkyf@mail.missouri.edu>
2018-09-19 21:52:05 -0500

    Add files via upload

---
+++ Capstone_Software.ino
@@ -0,0 +1,15 @@
+const int thermistorPin = 1;
+void setup() {
+  // put your setup code here, to run once:
+pinMode(thermistorPin,INPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+float ThermometerReading;
+ThermometerReading= analogRead(ThermistorPin); // read ADC Value directly.
+Reading = (1023/ThermometerReading)-1; 
+ThermometerReading = ResistorValue / ThermometerReading;
+Serial.println(ThermometerReading);
+}
