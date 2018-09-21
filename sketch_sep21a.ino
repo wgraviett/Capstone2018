edd8533da1037a5d01734336a746e5fe081c061f
wgraviett <wagkyf@mail.missouri.edu>
2018-09-21 18:56:32 -0500

    Adding Temperature Sensor Testing

---
+++ sketch_sep21a.ino
@@ -0,0 +1,13 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+int sensorValue = analogRead(A0);
+float temp = sensorValue*5/1024.0;
+temp=temp -0.5;
+temp = temp/0.01;
+Serial.println(sensorValue);
+}
