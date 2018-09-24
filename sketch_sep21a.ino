0af2e4109db9f6c82617fb35ad097a4eacfe4bd9
wgraviett <wagkyf@mail.missouri.edu>
2018-09-23 23:18:17 -0500

    Add files via upload
    correct version of temp changes.

---
+++ sketch_sep21a.ino
@@ -0,0 +1,19 @@
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+analogReference(EXTERNAL);
+}
+
+void loop() {
+  delay(5000);
+  // put your main code here, to run repeatedly:
+int sensorValue = analogRead(A0);
+int door = digitalRead(2);
+if (door == HIGH) {
+  Serial.println("Door Open");
+}
+float temp = (sensorValue-67)/1023.0 *5;
+temp=temp -0.5;
+temp = temp/0.01;
+Serial.println(temp); //should print 145 sensor valie with temp of 21c
+}
