f96a43a2aa98d542890a02a3c9dab15cd378d6b5
wgraviett <wagkyf@mail.missouri.edu>
2018-09-25 11:47:43 -0500

    Added Functionality of temp and door.

--- sketch_sep21a.ino
+++ sketch_sep21a.ino
@@ -2,18 +2,31 @@ void setup() {
   // put your setup code here, to run once:
 Serial.begin(9600);
 analogReference(EXTERNAL);
+  int sensorValue;
+  int door;
+  float temp;
 }
 
 void loop() {
   delay(5000);
-  // put your main code here, to run repeatedly:
-int sensorValue = analogRead(A0);
-int door = digitalRead(2);
-if (door == HIGH) {
-  Serial.println("Door Open");
+doorCheck();
+tempCheck();
 }
-float temp = (sensorValue-67)/1023.0 *5;
-temp=temp -0.5;
-temp = temp/0.01;
-Serial.println(temp); //should print 145 sensor valie with temp of 21c
+
+void doorCheck(){
+door = digitalRead(2); // Read pin 2 for door connection.
+  if (door ==HIGH){
+    Serial.println("Door Open");
+  }
+  else {
+    Serial.println("Door Closed");
+  }
+}
+
+void tempCheck(){
+  sensorValue = analogRead(A0); //Read ADC Value
+  temp =(sensorValue-67)/1023.0*5;
+  temp = temp -0.5;
+  temp = temp/0.01;
+  Serial.println(temp);//should print 145 sensor valie with temp of 21c
 }
