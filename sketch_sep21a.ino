f5f6d798b9fa3981a29e8c4e161f5ddc93c0f7fc
wgraviett <wagkyf@mail.missouri.edu>
2018-09-25 11:58:58 -0500

    Added RemoteConnectionReceiver, Changed pin to const.

--- sketch_sep21a.ino
+++ sketch_sep21a.ino
@@ -5,6 +5,10 @@ analogReference(EXTERNAL);
   int sensorValue;
   int door;
   float temp;
+  int zigbeestatus=0; //Returns 1 if both receivers are talking
+  const int RemoteZigbeeStatusPin=41;
+  const int DoorStatusPin = 2;
+  const int ThermisterPin =A0;
 }
 
 void loop() {
@@ -14,7 +18,7 @@ tempCheck();
 }
 
 void doorCheck(){
-door = digitalRead(2); // Read pin 2 for door connection.
+door = digitalRead(DoorStatusPin); // Read pin 2 for door connection.
   if (door ==HIGH){
     Serial.println("Door Open");
   }
@@ -24,9 +28,26 @@ door = digitalRead(2); // Read pin 2 for door connection.
 }
 
 void tempCheck(){
-  sensorValue = analogRead(A0); //Read ADC Value
+  sensorValue = analogRead(ThermisterPin); //Read ADC Value
   temp =(sensorValue-67)/1023.0*5;
   temp = temp -0.5;
   temp = temp/0.01;
   Serial.println(temp);//should print 145 sensor valie with temp of 21c
 }
+
+
+ int Remote_Connection_Receiver(){
+  // Using Digi Pin 8 connected to 5v. When zigbee on, 5v is being 
+    //provided to pin 8, which is replicated to other zigbee and used at arduino.  
+    //So If base zigbee shows zero then check power of other zigbee/comm channels. 
+    
+  
+  if (digitalRead(RemoteZigbeeStatusPin) == HIGH)
+     {
+    
+    zigbeestatus =1; 
+    
+     }
+    else zigbeestatus=0;
+  return zigbeestatus;
+  } 
