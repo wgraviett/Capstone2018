254cb3db5f4cb3952c8a9a37b046b388b85743b5
wgraviett <wagkyf@mail.missouri.edu>
2018-10-12 14:44:55 -0500

    added submission of rfid id
    Added submission of RFID ID to database. Currently commented out temp sending.

--- ItemIDSerialReadParse.ino
+++ ItemIDSerialReadParse.ino
@@ -1,24 +1,25 @@
 
 char ItemID[15];
+char ItemIDPrevious[15];
 int payload;
 int stp;
 
 String HOST = "192.168.137.1";
 String PORT = "80";
 String field = "field1";
-
+String strItemID="";
 int countTrueCommand;
 int countTimeCommand; 
 boolean found = false; 
 int valSensor = 1;
-
+int sendtowifi;
 
 void setup() {
   // put your setup code here, to run once:
 Serial2.begin(9600);
 Serial.begin(115200);
-  sendCommand("AT",5,"OK");
-  sendCommand("AT+CIPMUX=1",5,"OK");
+  //sendCommand("AT",5,"OK");
+ // sendCommand("AT+CIPMUX=1",5,"OK");
 stp=0;
 }
 
@@ -31,26 +32,50 @@ if(Serial2.available()>0){
 }
 if (payload==0x20 && stp !=1){ //Check for a space before keycode from remote unit. 
   Serial2.readBytesUntil("\r\n",ItemID,15); //Read keycode until newline character
-  //Serial.print(ItemID); //print out keycode array. 
+  ItemID[11]='\0';
+ // Serial.print(ItemID); //print out keycode array. 
+  sendtowifi=1;
   stp=1; //Allow for new codes to be read. 
 }
 
-//------------------------------------------------------------------------Wifi
+//------------------------------------------------------------------------Wifi - Send Temp
 valSensor = getSensorData();
  String getData = "GET /ESPDemoCode.php?temp="+String(valSensor)+"&door=1 HTTP/1.1\r\nHost: 192.168.137.1\r\nConnection: close\r\n";
 
+ //sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
+ //sendCommand("AT+CIPSEND=0," +String(getData.length()+2),4,">");
+ 
+ //sendCommand("AT+CIPSEND=0,92",4,">"); not used
+ //Serial.println(getData);delay(1500);countTrueCommand++;
+ //sendCommand("AT+CIPCLOSE=0",5,"OK");
+ //delay(4000);
+
+//-----------------------------Wifi - Send Item
+if (sendtowifi==1){
+  //Indicates that a new item has arrived, so send it to database. 
+strItemID =String(ItemID);
+Serial.print("in");
+String getData = "GET /ItemInsert.php?itemid="+strItemID+" HTTP/1.1\r\nHost: 192.168.137.1\r\nConnection: close\r\n";
  sendCommand("AT+CIPSTART=0,\"TCP\",\""+ HOST +"\","+ PORT,15,"OK");
  sendCommand("AT+CIPSEND=0," +String(getData.length()+2),4,">");
- //sendCommand("AT+CIPSEND=0,92",4,">");
  Serial.println(getData);delay(1500);countTrueCommand++;
  sendCommand("AT+CIPCLOSE=0",5,"OK");
- delay(60000);
+// delay(4000);
+sendtowifi=0;
 }
 
+
+
+
+ 
+} //Loop Close 
+
+
+//--------------------------------------------------------------------------------
 int getSensorData(){
   return random(1000); // Replace with 
 
-//--------------------------------------------------------------------------------
+
 }
 
 
