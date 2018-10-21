ca322214d5d4480a1b89f812a815e7d3f904811d
wgraviett <wagkyf@mail.missouri.edu>
2018-10-21 16:55:15 -0500

    Created Powermonitor insert. Insert into database.

---
+++ PowerMonitorInsert.php
@@ -0,0 +1,30 @@
+<?php
+//Creates new record as per request, Call using this wesleygdatabase.epizy.com/ESPDemoCode.php?temp=VALUE&door=VALUE
+    //Connect to database
+    $servername = "localhost";
+    $username = "testuser";
+    $password = "testuser";
+    $dbname = "espdemo";
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("Database Connection failed: " . $conn->connect_error);
+    }
+    //Get current date and time
+   // date_default_timezone_set('Asia/Kolkata');
+   // $d = date("Y-m-d");
+    //echo " Date:".$d."<BR>";
+   // $t = date("H:i:s");
+    if(!empty($_GET['power']))
+    {
+    	$power = $_GET['power'];
+	    $sql = "INSERT INTO powerstatus (power_status) VALUES ('$power')";
+		if ($conn->query($sql) === TRUE) {
+		    echo "OK";
+		} else {
+		    echo "Error: " . $sql . "<br>" . $conn->error;
+		}
+	}
+	$conn->close();
+?>
